3b87076e0f1a78e14558e30fee54ac919d5490de
RahmatNS <rahmat.nasution1993@gmail.com>
2016-11-14 17:00:29 +0700

    [REF]bou_slide_backend_ext:porting module to odoo 10 - 2

--- bou_slide_backend_ext/controllers/main.py
+++ bou_slide_backend_ext/controllers/main.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 import werkzeug
 import json
-from openerp.http import request
-from openerp import http
+from odoo.http import request
+from odoo import http
 import json
 
 class website_hobbou(http.Controller):
